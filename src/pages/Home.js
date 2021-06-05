d5ec4817c8f3255f8c8df004fa8b6470e8727ed6
Nicole <nicolekpacha@gmail.com>
2021-06-05 16:42:07 -0600

    Added date

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,9 +2,13 @@ import React, { useState } from 'react';
 import Form from '../components/Form';
 
 export default function Home(props) {
+	let currentDate = new Date().toLocaleDateString();
+	console.log(currentDate);
+
 	return (
 		<div className="HomePage">
 			<h1>Be the Best You</h1>
+			<p>{currentDate}</p>
 			<h3>
 				Strive to be the best version of yourself in life, and take steps each
 				day towards this. Ask yourself today- what actions have I done to be
