906e5ece3cb2b0792e54466738a10e4b010ec64e
Nicole <nicolekpacha@gmail.com>
2021-06-05 15:23:27 -0600

    Before push

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,15 @@
 import React, { useState } from 'react';
 
 export default function Home(props) {
-	return <div className="HomePage">This is the {props.page} page</div>;
+	return (
+		<div className="HomePage">
+			<h1>Be the Best You</h1>
+			<h3>
+				Strive to be the best version of yourself in life, and take steps each
+				day towards this. Ask yourself today- what actions have I done to be
+				closer to a better me?
+			</h3>
+			<h3>List three things you are grateful for:</h3>
+		</div>
+	);
 }
